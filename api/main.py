26dcaacbd58f0eadc893a964d92456e7f3dfb1b0
mohit-divtechnosoft <142472398+r6778g@users.noreply.github.com>
2025-10-13 17:15:44 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -298,6 +298,7 @@ async def github_webhook(request: Request):
 
             try:
                 review_json = query_openrouter_focused(filename, patch, status)
+                logger.info(reviwe_json)
                 review_json = review_json.replace('\n','')
                 parsed_review = json.loads(review_json)
                 all_reviews.append(parsed_review)
